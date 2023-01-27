fc93cda00299bb531c9425a8461b05ec43e0d900
Damian Golan <102601753+DGpoland@users.noreply.github.com>
2023-01-27 22:34:22 +0100

    Add files via upload

---
+++ z1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Damian" << endl << "29348" << endl << "PJatk";
+    return 0;
+}
---
+++ z2-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int zakres, i, j;
+    int suma_czesc = 0;
+    int suma_calk = 0;
+
+    cout << "Podaj zakres: ";
+    cin >> zakres;
+    for (i = 1; i <= zakres; i++) {
+        suma_czesc = 0;
+        for (j = 1; j <= i; j++) {
+            suma_czesc += j;
+        }
+        suma_calk += suma_czesc;
+    }
+
+    cout << suma_calk;
+
+    return 0;
+}
